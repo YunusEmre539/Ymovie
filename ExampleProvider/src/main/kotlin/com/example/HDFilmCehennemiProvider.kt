79c1e308e4979097a390bff292264de2a5c601be
YunusEmre539 <93152648+YunusEmre539@users.noreply.github.com>
2026-02-14 01:57:03 +0300

    Update HDFilmCehennemiProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/HDFilmCehennemiProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/HDFilmCehennemiProvider.kt
@@ -1,9 +1,52 @@
-class HDFilmCehennemiProvider : MainAPI() { 
-    override var mainUrl = "https://www.hdfilmcehennemi.nl"
-    override var name = "HDFilmCehennemi"
-    override val hasMainPage = true
-    override var lang = "tr"
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
+// HDFilmCehennemiProvider.kt
+package com.example
 
-    // Buraya arama, ana sayfa ve video linklerini çekme fonksiyonları gelecek
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+
+// Site: https://www.hdfilmcehennemi.nl
+class HDFilmCehennemiProvider : MainAPI() {
+    // Eklentinin adı (Cloudstream'de görünecek)
+    override var name: String = "HDFilmCehennemi.nl (TR)"
+
+    // Sitenin ana URL'si
+    override var mainUrl: String = "https://www.hdfilmcehennemi.nl"
+
+    // Bu eklentinin hangi dildeki içerikleri sağladığı
+    override var lang: String = "tr"
+
+    // Bu eklentinin hangi tür içerikleri sağladığı (Film, Dizi vb.)
+    override var mainTypes = setOf(TvType.Movie)
+
+    // Sitenin arama yapılacak URL'si
+    override suspend fun search(query: String): List<SearchResponse> {
+        // TODO: Arama işlevi eklenecek
+        return listOf()
+    }
+
+    // Ana sayfadaki film listelerini döndürür
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        // TODO: Ana sayfa içeriği eklenecek
+        return HomePageResponse(emptyList())
+    }
+
+    // Bir film veya dizinin detay sayfasını ayrıştırır
+    override suspend fun load(url: String): LoadResponse? {
+        // TODO: Detay sayfası ayrıştırma eklenecek
+        return null
+    }
+
+    // Seçilen bir bölüm veya filmin video linklerini bulur
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        // TODO: Video linki çekme işlevi eklenecek
+        return false
+    }
 }
